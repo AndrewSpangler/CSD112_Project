b2043facb7ec18209cc3d591c3d1e211652d6c79
AndrewSpangler <115903589+AndrewSpangler@users.noreply.github.com>
2023-04-05 13:39:55 -0700

    Update module.py

--- modules/index/module.py
+++ modules/index/module.py
@@ -1,17 +1,19 @@
 from flask import Blueprint, render_template
 
+priority = 0 # Always load this module first
+
 blueprint = Blueprint(
-    "index",
+    "Index",
     __name__,
     static_folder="static",
     template_folder="templates"
 )
 
-
-@blueprint.route("/")
-def index():
-    return render_template("index.html")
-
-
 def load(app):
+    @blueprint.route("/")
+    def index():
+        app.selected = "Index"
+        return render_template("index.html")
+
     app.register_blueprint(blueprint)
+    app.menu_items.append("Index")
