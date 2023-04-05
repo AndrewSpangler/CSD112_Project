138b8b80095946ade5dcda897a85fa98473df10a
AndrewSpangler <115903589+AndrewSpangler@users.noreply.github.com>
2023-04-05 13:40:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,26 @@ from flask import Flask
 blueprints = []
 
 app = Flask(__name__)
+# This list is populated by dynamic modules
+# It it used by templates to create a nav bar
+app.menu_items = []
+# This variable is updated when the user selects an item in the navigation bar
+app.selected = ""
 
 @app.route('/')
 def index():
     return 'Hello world!'
 
+# context processor which runs before any template is rendered
+# Provides the menu items and current selection
+@app.context_processor
+def provide_selection():
+    return dict(menu_items=app.menu_items, selected=app.selected)
+
 # Import dynamic modules
 modules = []
 for entry in os.scandir("modules"):
-    if not entry.isdir(): # Ignore files in modules folder
+    if entry.isdir(): # Ignore files in modules folder
         module = os.path.join("modules", entry.name, "module.py")
         if os.path.isfile(module):
             spec = importlib.util.spec_from_file_location(
