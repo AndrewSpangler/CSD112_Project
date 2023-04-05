e7f47acf3808926e9bb18492418dc24debb0bcf6
AndrewSpangler <115903589+AndrewSpangler@users.noreply.github.com>
2023-04-05 13:00:27 -0700

    Update module.py

--- modules/index/module.py
+++ modules/index/module.py
@@ -1,6 +1,17 @@
+from flask import Blueprint, render_template
+
+blueprint = Blueprint(
+    "index",
+    __name__,
+    static_folder="static",
+    template_folder="templates"
+)
 
 
+@blueprint.route("/")
+def index():
+    return render_template("index.html")
 
 
 def load(app):
-  pass
+    app.register_blueprint(blueprint)
